0c1f5431fed547fe37bc88dfcd423a5f826b813c
Pedro Guilherme Moreira <pedroguilhermemoreira@Pedros-MacBook-Air.local>
2023-08-18 16:51:37 +0100

    increase timeout for mobile 3

--- js/bundle.js
+++ js/bundle.js
@@ -137,7 +137,7 @@ coloquei a execução dos comandos dentro de uma função para não ter de a rep
         setTimeout(() => {
             document.getElementById("cmd").focus();
             document.getElementById("cmd").selectionEnd = 0;
-        }, 10); // o timeout é necessário para que o cursor da texarea fique na posição 0 em vez da 1
+        }, 100); // o timeout é necessário para que o cursor da texarea fique na posição 0 em vez da 1
     }
 
     window.addEventListener("click", function (evt) {
